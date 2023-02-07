6aa4d16c14ad33abb5bf710d39dc0c99628358d0
Saleem007 <sajb8722@gmail.com>
2023-02-07 23:33:26 +0530

    Adding few new prog

--- src/test/java/StringPrograms/CheckAnagramProgram.java
+++ src/test/java/StringPrograms/CheckAnagramProgram.java
@@ -6,8 +6,10 @@ public class CheckAnagramProgram {
     public static void main(String[] args) {
         String str1="Army";
         String str2="Mary";
+        System.out.println(str1.compareToIgnoreCase(str2));;
         char [] s1=str1.toLowerCase().toCharArray();
         char [] s2=str2.toLowerCase().toCharArray();
+
         Arrays.sort(s1);
         Arrays.sort(s2);
         System.out.println(Arrays.equals(s1, s2));
--- src/test/java/StringPrograms/ReverseEachWords.java
+++ src/test/java/StringPrograms/ReverseEachWords.java
@@ -1,5 +1,8 @@
 package StringPrograms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ReverseEachWords {
     public static void main(String[] args) {
         String str1="Java Programs on String";
@@ -8,6 +11,10 @@ public class ReverseEachWords {
         {
             str2=str2+str1.charAt(i);
         }
-        System.out.println(str2);
+        String [] d=str2.split(" ");
+        ArrayList ar=new ArrayList<>(Arrays.asList(d));
+        for(int i=ar.size()-1;i>=0;i--) {
+            System.out.print(ar.get(i));
+        }
     }
 }
